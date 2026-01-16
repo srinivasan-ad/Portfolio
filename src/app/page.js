eb19716ac3cfa594689a8734acddea8dfec04425
srinivasan-ad <forprogramingpurpose@gmail.com>
2026-01-16 22:20:09 +0530

    fun

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import HomePage from "@/components/Home";
 export default function Home() {
   return (
   //  <Construction/>
-   <HomePage/>
+  //  <HomePage/>
+  <div className="flex items-center justify-center h-screen w-full text-4xl font-bold">Hi Giri</div>
   );
 }
